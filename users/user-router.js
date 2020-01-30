2d6ef9a5d45cda295e088205d6eab8ed2cd8fa30
Code4Blessings <robinwarden@code4blessings.com>
2020-01-30 18:04:37 -0500

    Register endpoint tested successfully

--- users/user-router.js
+++ users/user-router.js
@@ -2,20 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 const Users = require('./user-model');
+const restricted = require('../auth/restricted-middleware');
 
-
-router.get('/',(req, res) => {
- Users.find()
-    .then(users => {
-        res.status(200).json(users)
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({ 
-            errorMessage: "Error retrieving the list of users", 
-            message: err.message
+router.get('/', restricted, (req, res) => {
+    Users.find()
+        .then(users => {
+            res.status(200).json(users)
         })
-    })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                errorMessage: "Error retrieving the list of users",
+                message: err.message
+            })
+        })
 })
 
 
